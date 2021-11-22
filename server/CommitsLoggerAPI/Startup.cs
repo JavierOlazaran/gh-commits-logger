a66b9bd2e50c13a3556b7a05791519d190580b96
Javier Olazaran <jolazaran@flexiti.com>
2021-11-22 18:39:17 -0300

    Added SignalR and MediatR

--- server/CommitsLoggerAPI/Startup.cs
+++ server/CommitsLoggerAPI/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using CommitsLoggerAPI.Core.SignalHub;
 
 namespace CommitsLoggerAPI
 {
@@ -28,6 +29,7 @@ namespace CommitsLoggerAPI
         {
 
             services.AddControllers();
+            services.AddSignalR();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CommitsLoggerAPI", Version = "v1" });
@@ -52,6 +54,7 @@ namespace CommitsLoggerAPI
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapHub<SignalHub>("");
                 endpoints.MapControllers();
             });
         }
